02f459c404593ac5dbaa7fdbde96dbff0e8bae84
Partha Roy <partharoypc@gmail.com>
2022-01-19 18:42:01 +0600

    3rd Project Complete Countdown Project using

--- 3rd-project/app.js
+++ 3rd-project/app.js
@@ -27,7 +27,15 @@ const giveaway = document.querySelector('.giveway');
 const dedline = document.querySelector('.dedline');
 const dedlineData = document.querySelectorAll('.dedline-data h1');
 
-let giveawayDay = new Date(2022, 4, 22, 10, 59, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+// months are ZERO index based;
+const giveawayDay = new Date(tempYear, tempMonth, tempDay + 15, 12, 30, 0);
+
+
+// let giveawayDay = new Date(2022, 1, 20, 10, 59, 0);
 
 const year = giveawayDay.getFullYear();
 const date = giveawayDay.getDate();
@@ -41,3 +49,46 @@ let weekday = giveawayDay.getDay();
 weekday = alldays[weekday];
 
 giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes} am`;
+
+// Total Time in milisecond
+const totalFutureTime = giveawayDay.getTime();
+
+function remainingTime() {
+    const todayDate = new Date().getTime();
+    const t = totalFutureTime - todayDate;
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    // calculate all values
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    // set values array
+    const values = [days, hours, minutes, seconds];
+
+    function format(item) {
+        if (item < 10) {
+            return (item = `0${item}`);
+        }
+        return item;
+    }
+    dedlineData.forEach(function(item, index) {
+        item.innerHTML = format(values[index]);
+    });
+
+    if (t < 0) {
+        clearInterval(countdown);
+        dedline.innerHTML = `<h1 class="expired">sorry, this giveaway has expired!</h1>`;
+    }
+
+
+}
+// countdown;
+let countdown = setInterval(totalFutureTime, 1000);
+//set initial values
+
+remainingTime();
